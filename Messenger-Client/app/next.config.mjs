1bab0ae93a3ad55137c611be9d4c7e3ad52cee06
valdimir-makarov <2010049@student.ruet.ac.bd>
2026-02-14 00:59:01 +0600

    yes

--- Messenger-Client/app/next.config.mjs
+++ Messenger-Client/app/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/auth/:path*',
+                destination: 'http://localhost:2021/:path*', // Proxy to Auth Service
+            },
+        ];
+    },
+};
 
 export default nextConfig;
