483d8ede5ea5f45b062ff7b1bb776443eb803a64
valdimir-makarov <2010049@student.ruet.ac.bd>
2026-02-14 03:40:53 +0600

    clend the Project

--- Complete-Golang-Micro-Service-Project/graphql/graph/model/model.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Account struct {
-	ID     string  `json:"id"`
-	Name   string  `json:"name"`
-	Orders []Order `json:"orders"`
-}
--- gRPC-GO/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/valdimir-makarov/Go-backend-Engineering/gRPC-GO
-
-go 1.24.0
